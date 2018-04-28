96c5389f1d4dc93cee96fc6f699efc3c2bf4164e
Erland <912859459@qq.com>
2018-04-28 11:15:26 +0800

    FiFo_modify

--- data_structure/FIFO_queue/FIFO_Array.h
+++ data_structure/FIFO_queue/FIFO_Array.h
@@ -10,26 +10,26 @@ static int N, headArray, tailArray;
 void InitQue(int Max)	//设置同时存在的最多数目Max
 {
 	N = Max + 1;
-		q = new Item[N]();
-			headArray = N;		//head从N开始，回卷后重合
-				tailArray = 0;		//tail从0开始
-				}
+	q = new Item[N]();
+	headArray = N;		//head从N开始，回卷后重合
+	tailArray = 0;		//tail从0开始
+}
 
-				bool EmptyQue()
-				{
-					return headArray%N == tailArray;
-					}
+bool EmptyQue()
+{
+	return headArray%N == tailArray;
+}
 
-					void PutQue(Item item)
-					{
-						q[tailArray++] = item;
-							tailArray = tailArray%N;
-							}
+void PutQue(Item item)
+{
+	q[tailArray++] = item;
+	tailArray = tailArray%N;
+}
 
-							Item GetQue()
-							{
-								headArray = headArray%N;
-									return q[headArray++];
-									}
+Item GetQue()
+{
+	headArray = headArray%N;
+	return q[headArray++];
+}
 
-									#endif
+#endif
